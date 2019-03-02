95b43814d52c07d7414188d649b4aa60ab3cc690
Mateusz Michalczyk <pan.michalczyk@gmail.com>
2019-03-02 16:26:52 +0100

    Add validation to component

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,21 @@ class App extends Component {
     userName: "",
     userEmail: "",
     userPassword: "",
-    regulations: false
+    regulations: false,
+
+    err: {
+      userName: false,
+      userEmail: false,
+      userPassword: false,
+      regulations: false
+    }
+  };
+
+  messages = {
+    userName_incorrect: "Your name is too short or consists space",
+    userEmail_incorrect: "You have to use @",
+    userPassword_incorrect: "Your password is to easy. Use min. 8 signs",
+    regulations_incorrect: "You have to accept regulations"
   };
 
   handleChange = e => {
@@ -27,7 +41,68 @@ class App extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("ok");
+
+    const validation = this.formValidation();
+    console.log(validation);
+    if (validation.correct) {
+      this.setState({
+        userName: "",
+        userEmail: "",
+        userPassword: "",
+        regulations: false,
+
+        err: {
+          userName: false,
+          userEmail: false,
+          userPassword: false,
+          regulations: false
+        }
+      });
+    } else {
+      this.setState({
+        err: {
+          userName: !validation.userName,
+          userEmail: !validation.userEmail,
+          userPassword: !validation.userPassword,
+          regulations: !validation.regulations
+        }
+      });
+    }
+  };
+
+  formValidation = () => {
+    // true - ok, false - not ok
+    let userName = false;
+    let userEmail = false;
+    let userPassword = false;
+    let regulations = false;
+    let correct = false;
+
+    if (
+      this.state.userName.length > 2 &&
+      this.state.userName.indexOf(" ") === -1
+    ) {
+      userName = true;
+    }
+    if (this.state.userEmail.indexOf("@") !== -1) {
+      userEmail = true;
+    }
+    if (this.state.userPassword.length >= 8) {
+      userPassword = true;
+    }
+    if (this.state.regulations) {
+      regulations = true;
+    }
+    if (userName && userEmail && userPassword && regulations) {
+      correct = true;
+    }
+    return {
+      userName,
+      userEmail,
+      userPassword,
+      regulations,
+      correct
+    };
   };
 
   render() {
@@ -42,6 +117,9 @@ class App extends Component {
             value={this.state.userName}
             onChange={this.handleChange}
           />
+          {this.state.err.userName && (
+            <span>{this.messages.userName_incorrect}</span>
+          )}
         </label>
         <label htmlFor="userEmail">
           Your email:
@@ -52,6 +130,9 @@ class App extends Component {
             value={this.state.userEmail}
             onChange={this.handleChange}
           />
+          {this.state.err.userEmail && (
+            <span>{this.messages.userEmail_incorrect}</span>
+          )}
         </label>
         <label htmlFor="userPassword">
           Your password:
@@ -62,6 +143,9 @@ class App extends Component {
             value={this.state.userPassword}
             onChange={this.handleChange}
           />
+          {this.state.err.userPassword && (
+            <span>{this.messages.userPassword_incorrect}</span>
+          )}
         </label>
         <label htmlFor="regulations">
           <input
@@ -72,6 +156,9 @@ class App extends Component {
             onChange={this.handleChange}
           />
           I have read the regulations
+          {this.state.err.regulations && (
+            <span>{this.messages.regulations_incorrect}</span>
+          )}
         </label>
         <button>Sign in</button>
       </form>
